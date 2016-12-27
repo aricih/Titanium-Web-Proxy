6236bf2ade078473147cff7754c32601f13ddad9
Hakan Arıcı <hakan@netsparker.com>
2016-12-27 18:06:33 +0300

    * UnitTests project's target framework is set to 4.6.1. * CompressionFactory is unit tested. * GZipCompressedByteArray is implemented to compare compressed data properly. * GZipCompression is unit tested.

---
+++ Tests/Titanium.Web.Proxy.UnitTests/Compression/GZipCompressionFixture.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using NUnit.Framework;
+using Titanium.Web.Proxy.Compression;
+using UnitTests.Titanium.Web.Proxy.Helpers;
+
+namespace Titanium.Web.Proxy.UnitTests.Compression
+{
+	[TestFixture]
+	public class GZipCompressionFixture
+	{
+		[TestCaseSource(typeof(GZipCompressionTestCaseSource), nameof(GZipCompressionTestCaseSource.CompressTestCases))]
+		public GZipCompressedByteArray Compress_works_properly(byte[] data)
+		{
+			var compression = new GZipCompression();
+			var result = compression.Compress(data).Result;
+
+			return result != null ? new GZipCompressedByteArray(result) : null;
+		}
+
+		private class GZipCompressionTestCaseSource
+		{
+			public static IEnumerable CompressTestCases
+			{
+				get
+				{
+					yield return new TestCaseData(null).Returns(null);
+					yield return new TestCaseData(Array.Empty<byte>()).Returns(GZipCompressedByteArray.Empty);
+					yield return new TestCaseData(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 }).Returns(new GZipCompressedByteArray(31, 139, 8, 0, 67, 166, 98, 88, 0, 255, 99, 96, 128, 0, 0, 105, 223, 34, 101, 8, 0, 0, 0));
+				}
+			}
+		}
+	}
+}
