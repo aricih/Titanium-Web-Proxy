347f5c398f058b39d5b2778c07579b51dff5cc0f
Hakan Arıcı <hakan@netsparker.com>
2017-01-11 12:55:30 +0300

    Fixes test name.

--- Tests/Titanium.Web.Proxy.UnitTests/Compression/ZlibCompressionFixture.cs
+++ Tests/Titanium.Web.Proxy.UnitTests/Compression/ZlibCompressionFixture.cs
@@ -32,7 +32,7 @@ namespace Titanium.Web.Proxy.UnitTests.Compression
 
                     yield return new TestCaseData(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 })
                         .Returns(new byte[] { 120, 156, 99, 96, 128, 0, 0, 0, 8, 0, 1 })
-                        .SetName("new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 }");
+                        .SetName("Compresses zero vector properly");
 
                     yield return new TestCaseData(new byte[] { 0, 1, 2, 4, 8, 16, 32, 64 })
                         .Returns(new byte[] { 120, 156, 99, 96, 100, 98, 225, 16, 80, 112, 0, 0, 0, 255, 0, 128 })
